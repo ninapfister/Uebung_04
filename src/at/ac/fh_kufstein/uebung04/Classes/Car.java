30d8cd48eac851e10ec9d5542d9ebdfd74f75d6b
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-12-06 09:59:57 +0100

    Aufgabe1

---
+++ src/at/ac/fh_kufstein/uebung04/Classes/Car.java
@@ -0,0 +1,15 @@
+package at.ac.fh_kufstein.uebung04.Classes;
+
+public class Car
+{
+    String color;
+    int wheels;
+    double speed;
+
+
+    public void currentSpeed()
+    {
+        System.out.println(" Die Geschwindigkeit beträgt " + this.speed);
+    }
+}
+
---
+++ src/at/ac/fh_kufstein/uebung_04/Aufruf.java
@@ -0,0 +1,9 @@
+package at.ac.fh_kufstein.uebung_04;
+
+public class Aufruf
+{
+    public static void main (String []args)
+    {
+
+    }
+}
